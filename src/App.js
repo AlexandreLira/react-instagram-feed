5f114322c38682466e6b534bda2a107beb261c1a
AlexandreLira <alexandrelira112@gmail.com>
2021-09-22 10:00:24 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Post from './components/Post'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {   
+
+    constructor(props){
+        super(props)
+        this.state = {
+            feed: [
+                {
+                    id: 1,
+                    userName: "Liam",
+                    profilePicture: 'https://img.wattpad.com/f994802e13a436e7394077013b5e05dd57e509a8/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f535f646e6651437268566e4267513d3d2d31342e313461373563306136323162633134393733333138343139343238372e6a7067',
+                    postImage: 'https://64.media.tumblr.com/31e1d3f7a6d5ebf7e263e49fde493bad/c4b4bb52b35d9a63-a9/s640x960/36efb8fcb5118f3f02c123f22ee12ddcd169149e.jpg',
+                    likes: 10,
+                    coments: 6,
+                },
+                {
+                    id: 2,
+                    userName: "Olivia",
+                    profilePicture: 'https://i.pinimg.com/originals/6e/e9/41/6ee941dd0d85066048be3753a070a608.jpg',
+                    postImage: 'https://i.pinimg.com/originals/ef/a3/08/efa308d0a87700049803b091c5a8dc40.jpg',
+                    likes: 190,
+                    coments: 18,
+                },
+                {
+                    id: 3,
+                    userName: "Benjamin",
+                    profilePicture: 'https://pbs.twimg.com/profile_images/1232678681383985152/m8rotkpo_400x400.jpg',
+                    postImage: 'https://64.media.tumblr.com/5589ee8aa93d0bc7c715cd135d0a28be/tumblr_pbh0xeaOsp1vu696ko1_1280.jpg',
+                    likes: 2,
+                    coments: 1,
+                },
+                {
+                    id: 3,
+                    userName: "Sophia",
+                    profilePicture: 'https://i.pinimg.com/originals/af/70/f5/af70f511081d394803a31ea1934cf1de.png',
+                    postImage: 'https://i.pinimg.com/originals/a9/cf/7a/a9cf7a66d14cce0555e367d812b11361.jpg',
+                    likes: 60,
+                    coments: 12,
+                }
+            ]
+            
+        }
+
+    }
+    
+    
+    render(){
+        return(
+            <div className="App">
+                {this.state.feed.map( item => <Post item={item}/> )}
+                
+            </div>
+        );
+    }
 }
-
 export default App;
---
+++ src/components/Post/index.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react'
+import './styles.css'
+
+class RenderHeader extends Component {
+    render(){
+        return(
+            <header className="Header">
+                <div>
+                    <img className="Header-profilePicture" src={this.props.profilePicture}/>
+                </div>
+                <div className="Header-container-profileName">
+                    <p className="Header-profileName">{this.props.name}</p>
+                </div>
+                
+                <svg aria-label="More options" class="_8-yf5 " color="#262626" fill="#262626" height="40" role="img" viewBox="0 0 24 24" width="40">
+                    <circle cx="12" cy="12" r="1.5"></circle>
+                    <circle cx="6.5" cy="12" r="1.5"></circle>
+                    <circle cx="17.5" cy="12" r="1.5"></circle>
+                </svg>
+               
+            </header>
+        )
+    }
+}
+
+class RenderPostImage extends Component {
+    render(){
+        return (
+            <div className="PostImage">
+                <img src={this.props.postImage}/>
+            </div>
+        )
+    }
+}
+
+class RenderPostFooter extends Component {
+    render(){
+        return(
+            <div className="PostFooter">
+                <div className="PostFooter-buttons">
+                    <span>
+                        <button type="button">
+                            <img className="PostFooter-button-icon" src="https://icons-for-free.com/iconfiles/png/512/heart-131965017458786724.png"/>
+                            <p>{this.props.likes}</p>
+                        </button>
+                    </span>
+                    <span>
+                        <button type="button">
+                            <img className="PostFooter-button-icon" src="https://static.thenounproject.com/png/638755-200.png"/>
+                            <p>{this.props.coments}</p>
+                        </button>
+                    </span>
+                    <span>
+                        <button type="button">
+                            <img className="PostFooter-button-icon" src="https://static.thenounproject.com/png/497450-200.png"/>
+                        </button>
+                    </span>
+                    <span className="PostFooter-save-icon">
+                        <button type="button">
+                            <img className="PostFooter-button-icon" src="https://static.thenounproject.com/png/50608-200.png"/>
+                            
+                        </button>
+                    </span>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Post extends Component {
+    render() {
+        let item = this.props.item
+        return (
+            <div className="Post" key={item.id}>
+                <RenderHeader 
+                    name={item.userName}
+                    profilePicture={item.profilePicture}
+                />
+                <RenderPostImage postImage={item.postImage}/>
+                <RenderPostFooter likes={item.likes} coments={item.coments}/>
+            </div>
+        )
+    }
+}
+
+export default Post
